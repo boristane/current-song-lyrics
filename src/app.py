3d35fedfad0ab83b39992043e42e22441ac7434a
Boris Tane <boris.tane@gmail.com>
2019-09-22 04:15:00 +0100

    Adds get-token route

--- src/app.py
+++ src/app.py
@@ -6,6 +6,6 @@ from flask import Flask, escape, request
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/public')
 
 import routes
--- src/routes.py
+++ src/routes.py
@@ -1,15 +1,18 @@
 from app import app
 from helpers import generate_random_string
-from flask import make_response, redirect
+from flask import make_response, redirect, request, jsonify
 from urllib.parse import urlencode
 from dotenv import load_dotenv
 from os import getenv
+import base64
+import requests
 
 load_dotenv()
 
 state_key = "spotify_auth_state"
 spotify_client_id = getenv("SPOTIFY_CLIENT_ID")
 spotify_redirect_uri = getenv("SPOTIFY_REDIRECT_URI")
+spotify_client_secret = getenv("SPOTIFY_CLIENT_SECRET")
 
 @app.route("/")
 @app.route("/index")
@@ -34,3 +37,28 @@ def login():
   resp.set_cookie(state_key, state)
 
   return resp
+
+
+@app.route("/get-token")
+def get_token():
+  code = request.args.get("code")
+  state = request.args.get("state")
+  stored_state = request.cookies.get(state_key)
+  if state is None or state != stored_state:
+    print({"error": "state_mismatch"})
+    return jsonify({"error": "state_mismatch"}), 500
+  auth = 'Basic ' + base64.b64encode((spotify_client_id + ':' + spotify_client_secret).encode('ascii')).decode("utf-8")
+  query = {
+    "code": code,
+    "redirect_uri": spotify_redirect_uri,
+    "grant_type": "authorization_code"
+  }
+  query_string = urlencode(query)
+  headers = {
+    "Authorization": auth,
+    "Content-Type": "application/x-www-form-urlencoded",
+  }
+  response = requests.post("https://accounts.spotify.com/api/token", data=query_string, headers=headers)
+  if response.status_code !== 200:
+    return jsonify({ "error": "invalid_token" }), 500
+  return response.json(), 200
